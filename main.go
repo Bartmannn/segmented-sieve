3057d068ffd89fa7828198fb52dcdb7210a246b0
Bartosz Bohdziewicz <bohdziewicz.bartosz@gmail.com>
2026-05-19 11:12:56 +0000

    Savings results to the file

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println(standardSieve(100))
-	fmt.Println(segmentedSieve(50, 100))
+	// fmt.Println(standardSieve(34787))
+	// fmt.Println(genericSegmentedSieve[uint32](2147483649, 4294967295))
+	// fmt.Println(genericSegmentedSieve[int8](5, 127))
+	saveToFile("primes.txt", genericSegmentedSieve[uint32](2147483649, 4294967295))
+}
+
+func saveToFile[T Number](filename string, primes []T) error {
+	fmt.Println("Saving...")
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	for _, p := range primes {
+		fmt.Fprintf(writer, "%b %d\n", p, p)
+	}
+
+	fmt.Println("Primes numbers are saved!")
+
+	return nil
 }
