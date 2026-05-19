3f5e6f661cd94afc8adb22233d79b3abf5550604
Bartosz Bohdziewicz <bohdziewicz.bartosz@gmail.com>
2026-05-19 15:03:30 +0000

    Auto-created ranges for numbers, based on bit-length numbers. Savings all primes numbers to file

--- main.go
+++ main.go
@@ -6,13 +6,6 @@ import (
 	"os"
 )
 
-func main() {
-	// fmt.Println(standardSieve(34787))
-	// fmt.Println(genericSegmentedSieve[uint32](2147483649, 4294967295))
-	// fmt.Println(genericSegmentedSieve[int8](5, 127))
-	saveToFile("primes.txt", genericSegmentedSieve[uint32](2147483649, 4294967295))
-}
-
 func saveToFile[T Number](filename string, primes []T) error {
 	fmt.Println("Saving...")
 
@@ -29,7 +22,46 @@ func saveToFile[T Number](filename string, primes []T) error {
 		fmt.Fprintf(writer, "%b %d\n", p, p)
 	}
 
-	fmt.Println("Primes numbers are saved!")
+	fmt.Println("Completed!")
 
 	return nil
+}
+
+func prepareRange[T Number](bits uint8) (T, T) {
+	var start T = (1 << (bits - 1)) | 1            // 100..001
+	var end T = (((1 << (bits - 1)) - 1) << 1) | 1 // 111..111
+	return start, end
+}
+
+func runSieve(bits uint8) error {
+	fmt.Println("Looking for prime numbers...")
+
+	if bits <= 8 {
+		start, end := prepareRange[uint8](bits)
+		var primes []uint8 = genericSegmentedSieve(start, end)
+		var filename string = fmt.Sprintf("primes_%db.txt", bits)
+		return saveToFile(filename, primes)
+	} else if bits <= 16 {
+		start, end := prepareRange[uint16](bits)
+		var primes []uint16 = genericSegmentedSieve(start, end)
+		var filename string = fmt.Sprintf("primes_%db.txt", bits)
+		return saveToFile(filename, primes)
+	} else if bits <= 32 {
+		start, end := prepareRange[uint32](bits)
+		var primes []uint32 = genericSegmentedSieve(start, end)
+		var filename string = fmt.Sprintf("primes_%db.txt", bits)
+		return saveToFile(filename, primes)
+	} else if bits > 64 {
+		return fmt.Errorf("%d cannot be handled!", bits)
+	}
+
+	start, end := prepareRange[uint64](bits)
+	var primes []uint64 = genericSegmentedSieve(start, end)
+	var filename string = fmt.Sprintf("primes_%bd.txt", bits)
+	return saveToFile(filename, primes)
+}
+
+func main() {
+	var bits uint8 = 32
+	runSieve(bits)
 }
